1c28e39e6c91724b7726da5c0acd7a9ee9103a37
CaptainFredric <captainarmoreddude@gmail.com>
2026-04-12 00:35:38 -0500

    da

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 let isModalOpen = false;
 let contrastToggle = false;
 
+const scaleFactor = 1/20;
 
 function toggleContrast(){
     contrastToggle = !contrastToggle;
@@ -53,6 +54,16 @@ function toggleModal(){
 }
 // Public key: FJycpJmjl6xe6BqjR
 
-function moveBackgorund(event){
+function moveBackground(event){
+ 
+const shapes = document.querySelectorAll(" .shape");
+const x = event.clientX * scaleFactor;
+const y = event.clientY * scaleFactor;
+
+for( let i = 0; i < shapes.length; ++i){
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+}
     
 }
